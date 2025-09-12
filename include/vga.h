d509282af573df56a8a74cdb6e3eca95adac13c2
IoannisCh <ioannis.chantolios@ieee.org>
2025-09-12 23:27:48 +0100

    updates on files to run  on VM

--- include/vga.h
+++ include/vga.h
@@ -1,8 +1,20 @@
 #ifndef VGA_H
 #define VGA_H
 
+#include <stdint.h>
+
+#define VGA_WIDTH 80
+#define VGA_HEIGHT 25
+
+extern int cursor_row;
+extern int cursor_col;
 void vga_init();
 void print_char(char c);
 void print_string(const char* str);
+
+void clear_screen();
+
+void update_cursor(int row, int col);
+void move_cursor_back();
 
 #endif
--- kernel/vga.c
+++ kernel/vga.c
@@ -1,30 +1,74 @@
 #include "../include/kernel.h"
+#include "../include/vga.h"
+#include "../include/io.h"
 
 unsigned short* VGA_BUFFER = (unsigned short*)0xB8000;
 
+int cursor_row = 0;
+int cursor_col = 0;
+
 void vga_clear_screen() {
-    for (int i = 0; i < 80 * 25; i++) {
+    for (int i = 0; i < VGA_WIDTH * VGA_HEIGHT; i++) {
         VGA_BUFFER[i] = (0x0F << 8) | ' ';  // White text on black
     }
+
+    cursor_row = 0;
+    cursor_col = 0;
+    update_cursor(cursor_row, cursor_col);
 }
 
-void vga_print(const char* str) {
-    int i = 0;
-    while (str[i]) {
-        VGA_BUFFER[i] = (0x0F << 8) | str[i];
-        i++;
+void update_cursor(int row, int col){
+    unsigned short pos = row * VGA_WIDTH + col;
+
+    outb(0x3D4, 0x0F);
+    outb(0x3D5, (unsigned char)(pos & 0xFF));
+    outb(0x3D4, 0x0E);
+    outb(0x3D5, (unsigned char)((pos >> 8) & 0xFF));
+}
+
+void scroll_screen(){
+    for (int row = 1; row < VGA_HEIGHT; row++){
+        for (int col = 0; col < VGA_WIDTH; col++){
+            VGA_BUFFER[(row - 1) * VGA_WIDTH] = VGA_BUFFER[row * VGA_WIDTH + col];
+        }
     }
+
+    for (int col = 0; col < VGA_WIDTH; col++){
+        VGA_BUFFER[(VGA_HEIGHT -1) * VGA_WIDTH + col] = (0x0F << 8) | ' ';
+    }
+
+    if (cursor_row > 0)
+        cursor_row--;
 }
 
 void print_char(char c) {
-    volatile char *video = (volatile char*)0xB8000;
-    *video = c;
+    if (c == '\n'){
+        cursor_row++;
+        cursor_col = 0;
+    } else if (c == '\b'){
+        move_cursor_back();
+    } else {
+        VGA_BUFFER[cursor_row * VGA_WIDTH + cursor_col] = (0x0F << 8) | c;
+        cursor_col++;
+    if (cursor_col >= VGA_WIDTH){
+        cursor_col = 0;
+        cursor_row++;
+    }
+    }
+
+    if (cursor_row >= VGA_HEIGHT){
+        scroll_screen();
+    }
+
+    update_cursor(cursor_row, cursor_col);
 }
 
-void print_string(const char *str) {
-    while (*str) {
+void print_string(const char* str){
+    while (*str)
+    {
         print_char(*str++);
     }
+    
 }
 
 void print_hex(uint8_t num){
@@ -38,8 +82,9 @@ void move_cursor_back(){
         cursor_col--;
     } else if (cursor_row > 0) {
         cursor_row--;
-        curso_col = VGA_WIDTH - 1;
+        cursor_col = VGA_WIDTH - 1;
     }
 
+    VGA_BUFFER[cursor_row * VGA_WIDTH + cursor_col] = (0x0F << 8) | ' ';
     update_cursor(cursor_row, cursor_col);
 }
