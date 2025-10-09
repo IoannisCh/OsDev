a7323ba40ce7acf2f1bcc58f0ccdfa65feeae3b6
IoannisCh <ioannis.chantolios@ieee.org>
2025-10-09 19:29:45 +0100

    improve keyboard.h

--- include/keyboard.h
+++ include/keyboard.h
@@ -1,6 +1,16 @@
 #ifndef KEYBOARD_H
 #define KEYBOARD_H
 
-void init_keyboard();
+#include <stdint.h>
+#include <stdbool.h>
+#include "../include/isr.h"
+
+void init_keyboard(void);
+
+bool keyboard_line_ready(void);
+
+int keyboard_get_line(char* dest, int maxlen);
+
+void keyboard_clear_line(void);
 
 #endif
